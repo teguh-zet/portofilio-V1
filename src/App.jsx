fef6c55184ffdc8ba753d24ea15bad36481a574b
teguh ahmadi zebua <zetteaz@gmail.com>
2025-07-10 01:24:56 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Navbar from './section/Navbar'
+import Hero from './section/Hero'
+import About from './section/About'
+
+const App = () => {
+  return (
+    <div className='container mx-auto max-w-7xl'>
+      {/*navbar*/}
+      <Navbar />
+      {/* hero */}
+      <Hero/>
+      {/* about */}
+      <About />
+      {/* projects */}
+      {/* experience */}
+      {/* testimonial */}
+      {/* contact */}
+      {/* footer */}
+
+
+    </div>
+  ) 
+}
+
+export default App 
